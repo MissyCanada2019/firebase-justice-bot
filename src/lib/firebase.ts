c2fa03b583059dea42aaf6adfd9545d556e7c0ff
Terri Bertin <terri.bertin57@gmail.com>
2025-07-08 18:34:30 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,4 +1,5 @@
-import { initializeApp, getApps, getApp } from 'firebase/app';
+import { getApp, getApps, initializeApp } from 'firebase/app';
+import { getAnalytics, isSupported } from 'firebase/analytics';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
@@ -7,12 +8,23 @@ const firebaseConfig = {
   projectId: "justicebotai",
   storageBucket: "justicebotai.firebasestorage.app",
   messagingSenderId: "259991262013",
-  appId: "1:259991262013:web:32a1e42fa3484e9a676b54"
+  appId: "1:259991262013:web:32a1e42fa3484e9a676b54",
+  measurementId: "G-GN30KPMDCH"
 };
 
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
+
+// Conditionally initialize Analytics only on the client side
+if (typeof window !== 'undefined') {
+    isSupported().then(supported => {
+        if (supported) {
+            getAnalytics(app);
+        }
+    });
+}
+
 
 export { app, auth };
