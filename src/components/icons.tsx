fc2e370aee40df62ae6cb8a36042fee485a1aeb9
Terri Bertin <terri.bertin57@gmail.com>
2025-07-10 00:32:46 +0000

    I still don’t see my logo anywhere. I’m not to sure why or what’s happen

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -4,22 +4,16 @@ export const Icons = {
   justiceBotLogo: (props: SVGProps<SVGSVGElement>) => (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 100 100"
+      viewBox="0 0 24 24"
+      fill="currentColor"
       {...props}
     >
-      <rect width="100" height="100" rx="15" fill="hsl(var(--primary))" />
-      <text
-        x="50"
-        y="50"
-        fontFamily="sans-serif"
-        fontSize="50"
-        fill="hsl(var(--primary-foreground))"
-        textAnchor="middle"
-        dy=".3em"
-        fontWeight="bold"
-      >
-        JB
-      </text>
+      <path d="M12 1a2 2 0 0 0-2 2v2H8a2 2 0 1 0 0 4h1.59a6.5 6.5 0 0 0 12.82 0H24a2 2 0 1 0 0-4h-2V3a2 2 0 0 0-2-2h-8zm-2 9a4.5 4.5 0 0 1 8.95 0H10zM4.75 16.5a.75.75 0 0 0-1.5 0v3a.75.75 0 0 0 1.5 0v-3zM20.75 16.5a.75.75 0 0 0-1.5 0v3a.75.75 0 0 0 1.5 0v-3z" />
+      <path d="M12 11c-2.48 0-4.5 2.02-4.5 4.5V23h9v-7.5c0-2.48-2.02-4.5-4.5-4.5zm-2.5 5.5a.5.5 0 0 1 1 0v3a.5.5 0 0 1-1 0v-3zm2 0a.5.5 0 0 1 1 0v3a.5.5 0 0 1-1 0v-3zm2 0a.5.5 0 0 1 1 0v3a.5.5 0 0 1-1 0v-3z" />
+      <path d="M7 13.25a.75.75 0 0 0-1.5 0v1.5a4 4 0 0 0 8 0v-1.5a.75.75 0 0 0-1.5 0v1.5a2.5 2.5 0 0 1-5 0v-1.5z" />
+      <path d="M17 13.25a.75.75 0 0 0-1.5 0v1.5a4 4 0 0 0 8 0v-1.5a.75.75 0 0 0-1.5 0v1.5a2.5 2.5 0 0 1-5 0v-1.5z" />
+      <circle cx="4" cy="9" r="2" />
+      <circle cx="20" cy="9" r="2" />
     </svg>
   ),
   mapleLeaf: (props: SVGProps<SVGSVGElement>) => (
